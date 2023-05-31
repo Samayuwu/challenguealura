de4a32d1630d4f08ed9a739422413ee2bf9e1d88
samay <rinconsamus@gmail.com>
2023-05-30 23:36:32 -0600

    alura challenge 1

---
+++ script.js
@@ -0,0 +1,71 @@
+var botonencriptar= document.querySelector(".btnencriptar");
+var botondesencriptar= document.querySelector(".btndesencriptar");
+var muneco= document.querySelector(".muneco");
+var contenedor= document.querySelector(".contenedorparrafo");
+var resultado= document.querySelector(".textoresultado");
+
+botonencriptar.onclick - encriptar;
+botonencriptar.onclick - desencriptar;
+
+function encriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = encriptarTexto(cajatexto);
+}
+
+function desencriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = desencriptarTexto(cajatexto);
+}
+
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".cajatexto")
+    return cajatexto.value
+}
+
+function ocultarAdelante(){
+    muneco.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i <texto.length; i++){
+        if(texto[i]== "a"){
+            textoFinal = textoFinal + "ai"
+            i = i+1;
+        }
+
+    else if(texto[i]== "e"){
+            textoFinal = textoFinal + "enter"
+            i = i+4;  
+    }
+    else if(texto[i]== "i"){
+     textoFinal = textoFinal + "imes"
+     i = i+3;
+    }
+    else if(texto[i]== "o"){
+    textoFinal = textoFinal + "ober"
+    i = i+3;
+    }
+    else if(texto[i]== "u"){
+    textoFinal = textoFinal + "ufat"
+    i = i+3;
+
+    }
+    else{
+        textoFinal = textoFinal + texto[i]
+    }
+}
+    return textoFinal;
+ }
+
+     const btnCopiar = document.querySelector(".btn-copiar");
+     btnCopiar.addEventListener("click", copiar = () => {
+        var contenido = document.querySelector(".texto-resultado").textContent;
+        navigator.clipboard.writeText(contenido);
+        console.log("hola");
+     })
